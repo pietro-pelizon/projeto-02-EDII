4bfc456d1d456dd1b0afd94acc7b95fecac9a09e
pietro-pelizon <pietrofernandopelizon@gmail.com>
2026-05-18 10:00:35 -0300

    fix(grafo): Correções de arquitetura e organização
    Inicializador do grafo agora recebe as funções que serão utilizadas ao longo de sua vida útil. Não mais recebe o número de vértices pois o grafo cresce dinamicamente
    
    funções de adição de vértice e arestas não recebem mais o vértice/aresta já criados, e sim um ID para ser associado a ele. A criação é feita dentro da função de adição

--- include/grafo.h
+++ include/grafo.h
@@ -1,31 +1,37 @@
 #ifndef PROJETO_02_EDII_GRAFO_H
 #define PROJETO_02_EDII_GRAFO_H
 
-typedef struct stVertex t_vertex;
-typedef struct stEdge t_edge;
-typedef struct stGraph t_graph;
+typedef struct stVertex vertex_t;
+typedef struct stEdge edge_t;
+typedef struct stGraph graph_t;
 
 #include <stdbool.h>
 #include "lista.h"
 
 /// @brief Inicializa um grafo com n vertices
+/// @param comparator
+/// @param destructor_edge_data
+/// @param destructor_vertex_data
 /// @return Retorna um ponteiro para o grafo criado
-t_graph *graph_init(int n);
+graph_t *graph_init(void (*comparator)(void *a, void *b),
+    void (*destructor_edge_data)(void *data),
+    void (*destructor_vertex_data)(void *data));
 
-bool graph_add_vertex(t_graph *g, t_vertex *v);
 
-bool graph_add_edge(t_graph *g, t_edge *a);
+bool graph_add_vertex(graph_t *g, void *data, const char *vertex_id);
 
-bool is_adjacente(t_graph *g, const char *id_v, const char *id_u);
+bool graph_add_edge(graph_t *g, void *data, const char *src_id, const char *target_id);
 
-t_vertex *graph_get_vertex(t_graph *g, const char *id_vertice);
+bool is_adjacente(graph_t *g, const char *id_v, const char *id_u);
 
-t_edge *graph_remove_edge(t_graph *g, const char *id_origem, const char *id_destino);
+vertex_t *graph_get_vertex(graph_t *g, const char *id_vertice);
 
-t_vertex *graph_remove_vertex(t_graph *g, const char *id_vertice);
+edge_t *graph_remove_edge(graph_t *g, const char *id_origem, const char *id_destino);
 
-lista *graph_get_neighbors(t_graph *g, const char *id_vertice);
+vertex_t *graph_remove_vertex(graph_t *g, const char *id_vertice);
 
-void graph_destroy(t_graph *g);
+lista_t *graph_get_neighbors(graph_t *g, const char *id_vertice);
+
+void graph_destroy(graph_t *g);
 
 #endif //PROJETO_02_EDII_GRAFO_H
