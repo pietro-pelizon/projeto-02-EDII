1b93d84b31fd9b5daa70db79d958046444d1ad5c
pietro-pelizon <pietrofernandopelizon@gmail.com>
2026-05-20 22:57:01 -0300

    feat(rua): Usando strdup que faz alocamento dinâmico, checagem de tamanho e duplicação da string internamente -> Melhor que utilizar malloc(strlen(str)) e depois strncpy

--- src/rua.c
+++ src/rua.c
@@ -10,8 +10,8 @@ typedef struct stRua {
     char *cep_esquerda; // CEP quadra a esquerda
     char *cep_direita;  // CEP quadra a direita
     char *nome;
-    double vm;          // Velocidade média
-    double cmp;         // Comprimento da rua
+    double vm;          // Velocidade média (m/s)
+    double cmp;         // Comprimento da rua (metros)
 } rua_t;
 
 rua_t *rua_init(const char *cep_esquerda, const char *cep_direita, const char *nome, double vm, double cmp) {
@@ -20,14 +20,10 @@ rua_t *rua_init(const char *cep_esquerda, const char *cep_direita, const char *n
     rua_t *r = malloc(sizeof(rua_t));
     assert(r != NULL);
 
-    r -> cep_esquerda = malloc(strlen(cep_esquerda) + 1);
-    strncpy(r -> cep_esquerda, cep_esquerda, strlen(cep_esquerda) + 1);
-
-    r -> cep_direita = malloc (strlen(cep_direita) + 1);
-    strncpy(r -> cep_direita, cep_direita, strlen(cep_direita) + 1);
-
-    r -> nome = malloc(strlen(nome) + 1);
-    strncpy(r -> nome, nome, strlen(nome) + 1);
+    // Lembrar que strdup faz malloc implicitamente
+    r -> cep_esquerda = strdup(cep_esquerda);
+    r -> cep_direita = strdup(cep_direita);
+    r -> nome = strdup(nome);
 
     r -> vm = vm;
     r -> cmp = cmp;
@@ -42,8 +38,11 @@ char *get_cep_esquerda(rua_t *r) {
 void set_cep_esquerda(rua_t *r, char *novo_cep_esquerda) {
     assert(novo_cep_esquerda != NULL);
 
-    r -> cep_esquerda = malloc(strlen(novo_cep_esquerda) + 1);
-    strncpy(r -> cep_esquerda, novo_cep_esquerda, strlen(novo_cep_esquerda) + 1);
+    if (r -> cep_esquerda != NULL) {
+        free(r -> cep_esquerda);
+    }
+
+    r -> cep_esquerda = strdup(novo_cep_esquerda);
 }
 
 char *get_cep_direita(rua_t *r) {
@@ -53,8 +52,11 @@ char *get_cep_direita(rua_t *r) {
 void set_cep_direita(rua_t *r, char *novo_cep_direita) {
     assert(novo_cep_direita != NULL);
 
-    r -> cep_direita = malloc(strlen(novo_cep_direita) + 1);
-    strncpy(r -> cep_direita, novo_cep_direita, strlen(novo_cep_direita) + 1);
+    if (r -> cep_direita != NULL) {
+        free(r -> cep_direita);
+    }
+
+    r -> cep_direita = strdup(novo_cep_direita);
 }
 
 char *get_nome(rua_t *r) {
@@ -64,8 +66,11 @@ char *get_nome(rua_t *r) {
 void set_nome(rua_t *r, char *novo_nome) {
     assert(novo_nome != NULL);
 
-    r -> nome = malloc(strlen(novo_nome) + 1);
-    strncpy(r -> nome, novo_nome, strlen(novo_nome) + 1);
+    if (r -> nome != NULL) {
+        free(r -> nome);
+    }
+
+    r -> nome = strdup(novo_nome);
 }
 
 double get_vm(rua_t *r) {
