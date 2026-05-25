9b71bb94a9a1a7ad9ea7c3ead5579454c831dad3
pietro-pelizon <pietrofernandopelizon@gmail.com>
2026-05-24 22:05:31 -0300

    docs(via_handler)

---
+++ include/via_handler.h
@@ -0,0 +1,12 @@
+#ifndef PROJETO_02_EDII_VIA_HANDLER_H
+#define PROJETO_02_EDII_VIA_HANDLER_H
+
+#include "grafo.h"
+
+/// @brief Lê o arquivo (.via) e processa os dados
+/// conforme a necessidade
+/// @param path_via Caminho para o arquivo (.via)
+/// @param g Ponteiro para o grafo
+void via_handler(char *path_via, graph_t *g);
+
+#endif //PROJETO_02_EDII_VIA_HANDLER_H
