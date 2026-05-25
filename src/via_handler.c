193519f8056d6a65c9318330de548f7a5ce2a988
pietro-pelizon <pietrofernandopelizon@gmail.com>
2026-05-24 21:30:45 -0300

    feat(via_handler): Módulo de processamento do arquivo (.via)
    Necessitei da criação de uma nova struct, esquina_t para encapsular os dados que serão guardados pelo vértice
    
    O módulo é tão simples que dispensa apresentações

---
+++ src/via_handler.c
@@ -0,0 +1,87 @@
+#include "../include/via_handler.h"
+#include "../include/grafo.h"
+#include "../include/quadra.h"
+#include "../include/rua.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/exhash.h"
+
+// já que o campo de dados do vértice é genérico,
+// precisamos dessa struct para encapsular as coordenadas
+// atribuídas a ele
+typedef struct stEsquina_t {
+    double x, y;
+}esquina_t;
+
+
+static void comando_v(char *linha_lida, graph_t *g);
+static void comando_e(char *linha_lida, graph_t *g);
+
+void via_handler(char *path_via, graph_t *g) {
+    assert(path_via != NULL && g != NULL);
+
+    FILE *arquivo_via = fopen(path_via, "r");
+    assert(arquivo_via);
+
+    char linha_leitura[512];
+    int nv = 0;
+
+    fscanf(arquivo_via, "%d\n", &nv);
+    graph_set_nv(g, nv);
+
+    while (fgets(linha_leitura, sizeof(linha_leitura), arquivo_via)) {
+
+
+        char comando[2];
+        sscanf(linha_leitura, "%1s", comando);
+
+        if (strcmp(comando, "v") == 0) {
+            comando_v(linha_leitura, g);
+        }
+
+        if (strcmp(comando, "e") == 0) {
+            comando_e(linha_leitura, g);
+        }
+    }
+
+    fclose(arquivo_via);
+}
+
+static void comando_v(char *linha_lida, graph_t *g) {
+    assert(g);
+
+    char id[16];
+    double x, y;
+
+    sscanf(linha_lida, "v %15s %lf %lf", id, &x, &y);
+
+    esquina_t *e = malloc(sizeof(esquina_t));
+    assert(e);
+
+    e -> x = x;
+    e -> y = y;
+
+    graph_add_vertex(g, e, id);
+
+}
+
+static void comando_e(char *linha_lida, graph_t *g) {
+    char src_id[16], target_id[16], cep_esq[16], cep_dir[16], nome[128];
+    double cmp, vm;
+
+    // %[^\r\n] captura tudo (incluindo espaços) até a quebra de linha
+    sscanf(linha_lida, "e %15s %15s %15s %15s %lf %lf %[^\r\n]", src_id, target_id, cep_dir, cep_esq, &cmp, &vm, nome);
+
+    rua_t *r = rua_init(cep_esq, cep_dir, nome, vm, cmp);
+    assert(r);
+
+    graph_add_edge(g, r, src_id, target_id, nome);
+
+}
+
+
