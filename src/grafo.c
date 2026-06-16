c251d0deddff0f046789a5bcf00d9edeed267e97
pietro-pelizon <pietrofernandopelizon@gmail.com>
2026-06-15 23:44:31 -0300

    feat(dijkstra): Adiciona implementação do algoritmo de Dijkstra utilizando priority queue e hashmaps
    fix: A implementação já veio com bugs corrigidos porque esqueci de commitar:
    
    hashmaps utilizados estavam sendo inicializados com "bucket_size" com tamanho de "total_vertices", o que deixava o hashmap lento e em alguns casos poderia até mesmo travar todo o programa por "total_vertices" ser 0
    
    "custo_lido" e "custo_vizinho_lido" eram passados para "exhash_search" como "double **", mesmo erro previamente visto em outras funções
    
    no trecho "if (novo_custo < custo_vizinho)", id_vizinho não era removido e reinserido com novo custo
    
    Os IDs do hashmap dos pais não era liberados, causando memory leak. Foi resolvido criando uma função auxiliar para libera-las

--- src/grafo.c
+++ src/grafo.c
@@ -1,7 +1,7 @@
 #include "../include/grafo.h"
 
 #include <assert.h>
-
+#include <math.h>
 #include "../include/exhash.h"
 #include "../include/lista.h"
 
@@ -9,7 +9,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "priority_queue.h"
+#include "rua.h"
 #include "utils.h"
+
+
+#define EXHASH_BUCKET_BYTES(record_size, n_entries) \
+((sizeof(uint64_t) + (record_size)) * (n_entries) + sizeof(uint16_t) * 2)
 
 // ======= DECLARAÇÕES STATIC ========
 
@@ -30,7 +36,7 @@ typedef struct stContext{
 typedef struct stVertex {
     char *id;
     void *data;
-    lista_t *adjacent;
+    list_t *adjacent;
 
 } vertex_t;
 
@@ -88,7 +94,7 @@ bool graph_add_vertex(graph_t *g, void *data, const char *id) {
     strcpy(new_vertex -> id, id);
 
     new_vertex -> data = data;
-    new_vertex -> adjacent = init_lista();
+    new_vertex -> adjacent = list_init();
 
     // Adiciona um ponteiro do novo vértice ao hashmap do grafo
     exhash_insert(g -> vertices, &new_vertex, id);
@@ -121,7 +127,7 @@ bool graph_add_edge(graph_t *g, void *data, const char *src_id, const char *targ
     if (!src_v || !target_v) return false;
 
     // Checa se já existe uma aresta idêntica, caso exista, não cria nova aresta
-    if (search_lista(src_v -> adjacent, ((void *)target_id), cmp_target_edge)) {
+    if (list_search(src_v -> adjacent, ((void *)target_id), cmp_target_edge)) {
         return false;
     }
 
@@ -136,7 +142,7 @@ bool graph_add_edge(graph_t *g, void *data, const char *src_id, const char *targ
     new_edge -> data = data;
 
     // Nova aresta parte de src_v para target_v
-    insert_tail(src_v -> adjacent, new_edge);
+    list_push_back(src_v -> adjacent, new_edge);
 
     return true;
 }
@@ -155,7 +161,7 @@ bool is_adjacente(graph_t *g, const char *id_v, const char *id_u) {
     if (!v || !u) return false;
 
     // Procura na lista de adjacência de V se existe o vértice U
-    if (search_lista(v -> adjacent, ((void *)id_u), cmp_target_edge)) {
+    if (list_search(v -> adjacent, ((void *)id_u), cmp_target_edge)) {
         return true;
     }
 
@@ -184,7 +190,7 @@ edge_t *graph_get_edge(graph_t *g, const char *src_id, const char *target_id) {
     if (!src_v) return NULL;
 
     // Busca a aresta na lista dele usando a função de procurar na lista
-    return search_lista(src_v -> adjacent, (void *)target_id, cmp_target_edge);
+    return list_search(src_v -> adjacent, (void *)target_id, cmp_target_edge);
 }
 
 
@@ -195,7 +201,7 @@ bool graph_remove_edge(graph_t *g, const char *src_id, const char *target_id) {
     exhash_search(g -> vertices, src_id, &src_v);
     if (!src_v) return false;
 
-    edge_t *e = remove_first_data(src_v -> adjacent, (void *)target_id, cmp_target_edge);
+    edge_t *e = list_remove_first(src_v -> adjacent, (void *)target_id, cmp_target_edge);
     if (!e) return false;
 
     free(e -> target_id);
@@ -222,7 +228,7 @@ bool graph_remove_vertex(graph_t *g, const char *vertex_id) {
     exhash_foreach(g -> vertices, remove_neighbors_edges, &context);
 
     // Itera pela sua lista de adjacência destruindo as arestas
-    free_lista(v -> adjacent, g -> destructor_edge_data);
+    list_free(v -> adjacent, g -> destructor_edge_data);
 
     // Remove id e dados
     free(v -> id);
@@ -241,7 +247,7 @@ bool graph_remove_vertex(graph_t *g, const char *vertex_id) {
     return true;
 }
 
-lista_t *graph_get_neighbors(graph_t *g, const char *vertex_id) {
+list_t *graph_get_neighbors(graph_t *g, const char *vertex_id) {
     assert(g != NULL);
 
     vertex_t *v = graph_get_vertex(g, vertex_id);
@@ -257,7 +263,7 @@ void graph_destroy(graph_t *g) {
     exhash_foreach(g -> vertices, vertex_destroy_internal, g);
 
     // Destrói a estrutura raiz do Hash Map (Diretório e Baldes)
-    exhash_destroy(g -> vertices);
+    exhash_destroy(g -> vertices, NULL);
 
     // Libera a raiz do grafo
     free(g);
@@ -278,7 +284,7 @@ exhash_t *graph_get_exhash(graph_t *g) {
 
 // Struct para poder passar as informações para o graph_foreach_vertex
 typedef struct {
-    void (*user_callback)(const char *, void *, lista_t *, void *);
+    void (*user_callback)(const char *, void *, list_t *, void *);
     void *user_ctx;
 } internal_ctx_t;
 
@@ -295,7 +301,7 @@ static void internal_foreach_wrapper(void *record_data, void *context) {
 
 
 // Função pública para iterar sobre os vértices sem quebrar a opacidade
-void graph_foreach_vertex(graph_t *g, void (*callback)(const char *id, void *vertex_data, lista_t *adjacent, void *context), void *context) {
+void graph_foreach_vertex(graph_t *g, void (*callback)(const char *id, void *vertex_data, list_t *adjacent, void *context), void *context) {
     assert(g && g -> vertices && callback);
 
     // Ponte para os outros módulos
@@ -343,7 +349,7 @@ static void vertex_destroy_internal(void *record_data, void *context) {
 
     // Destrói todas as arestas que saem deste vértice
     if (v -> adjacent) {
-        free_lista(v -> adjacent, edge_destroy_internal);
+        list_free(v -> adjacent, edge_destroy_internal);
     }
 
     // Destrói o payload do vértice (struct quadra/coordenadas)
@@ -363,7 +369,7 @@ static void remove_neighbors_edges(void *record_data, void *context) {
     ctx_remove_t *ctx = (ctx_remove_t *)context;
 
     // Tenta remover a aresta que aponta para o vértice que está morrendo
-    edge_t *aresta_removida = remove_first_data(v -> adjacent, (void *)ctx -> target_id, cmp_target_edge);
+    edge_t *aresta_removida = list_remove_first(v -> adjacent, (void *)ctx -> target_id, cmp_target_edge);
 
     if (aresta_removida) {
         free(aresta_removida -> target_id);
@@ -372,6 +378,132 @@ static void remove_neighbors_edges(void *record_data, void *context) {
         }
         free(aresta_removida);
     }
+}
+
+static void libera_string_do_pais(void *record_data, void *context) {
+    (void)context;
+    // record_data aponta para dentro do bucket onde está guardado o char*
+    char *str = *(char **)record_data;
+    free(str);
+}
+
+static void libera_pais(exhash_t *pais) {
+    exhash_foreach(pais, libera_string_do_pais, NULL);
+    exhash_destroy(pais, NULL);
+}
+
+list_t *dijkstra(graph_t *g, bool flag_tempo, char *id_src,  char *id_dst) {
+    assert(g != NULL);
+
+    // Inicializando as estruturas necessárias para o algoritmo
+    exhash_t *custos = exhash_init(sizeof(double), EXHASH_BUCKET_BYTES(sizeof(double), 8));
+    assert(custos != NULL);
+    exhash_t *pais = exhash_init(sizeof(char *), EXHASH_BUCKET_BYTES(sizeof(char *), 8));
+    assert(pais != NULL);
+    pqueue_t *min_heap = pq_init(16);
+    assert(min_heap != NULL);
+
+
+    double custo_zero = 0.0;
+    exhash_insert(custos, &custo_zero, id_src);
+    pq_enqueue(min_heap, id_src, 0);
+
+
+    // Loop principal
+    while (!pq_is_empty(min_heap)) {
+        char *id_esquina = pq_dequeue(min_heap);
+
+        // Se o id retirado é o id do destino
+        // temos o melhor caminho
+        if (strcmp(id_esquina, id_dst) == 0) {
+            free(id_esquina);
+            break;
+        }
+
+        // Temos que olhar para cada vértice adjacente a esse
+        // e checar se o caminho é melhor
+        list_t *vizinhos = graph_get_neighbors(g, id_esquina);
+
+        double custo_atual = 0.0;
+        double custo_lido = 0.0;
+
+        // Buscando o custo acumulado para chegar no vértice atual
+        bool found_esquina = exhash_search(custos, id_esquina, &custo_lido);
+
+        // Se achou, esse será o custo atual
+        custo_atual = found_esquina ? custo_lido : custo_atual;
+
+        // Loop para olhar todos os vértices adjacentes
+        for (list_node_t *no = list_node_front(vizinhos); no != NULL; no = list_node_next(no)) {
+
+            // Obtendo o payload (dado interno) da aresta
+            edge_t *aresta = list_node_data(no);
+            rua_t *rua = edge_get_data(aresta);
+
+            // Temos que olhar para seu vizinho e descobrir
+            // qual o custo para chegar nele
+            const char *id_vizinho = edge_get_target_id(aresta);
+
+            // Se a flag tempo for true, o peso será o tempo mínimo do trajeto
+            // se não, o peso será o comprimento
+            double peso = flag_tempo ? rua_get_comprimento(rua)/rua_get_velocidade_media(rua) : rua_get_comprimento(rua);
+
+            // Cálculo do novo custo
+            double novo_custo = custo_atual + peso;
+
+            // Inicializa o custo para ir ao vizinho com infinito
+            double custo_vizinho = INFINITY;
+
+            double custo_vizinho_lido = 0.0;
+            bool found_vizinho = exhash_search(custos, id_vizinho, &custo_vizinho_lido);
+
+            custo_vizinho = found_vizinho ? custo_vizinho_lido : custo_vizinho;
+
+            // Teste principal: O custo para chegar a esse novo vizinho
+            // é menor do que o menor custo conhecido até agora?
+
+            if (novo_custo < custo_vizinho) {
+                exhash_remove(custos, id_vizinho);
+                exhash_insert(custos, &novo_custo, id_vizinho);
+
+                char *copia_id_esquina = my_strdup(id_esquina);
+                exhash_remove(pais, id_vizinho);
+                exhash_insert(pais, &copia_id_esquina, id_vizinho);
+                pq_enqueue(min_heap, id_vizinho, novo_custo);
+            }
+        }
+        free(id_esquina);
+    }
+
+    list_t *caminho = list_init();
+
+    // Teste para ver se foi possível alcançar o destino
+    if (exhash_search(custos, id_dst, NULL) == false) {
+        pq_destroy(min_heap);
+        exhash_destroy(custos, NULL);
+        libera_pais(pais);
+        return caminho;
+    }
+
+    // Backtracking
+    const char *atual = id_dst;
+    while (atual != NULL) {
+        list_push_front(caminho, my_strdup(atual));
+
+        // O backtracking se conclui ao chegarmos na origem
+        if (strcmp(atual, id_src) == 0) {
+            break;
+        }
+
+        exhash_search(pais, atual, &atual);
+    }
+
+    pq_destroy(min_heap);
+    exhash_destroy(custos, NULL);
+    libera_pais(pais);
+
+    return caminho;
+
 }
 
 
