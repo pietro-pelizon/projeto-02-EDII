54b5fd9bef20aeb95184c85a3e1e7dfa45d91116
pietro-pelizon <pietrofernandopelizon@gmail.com>
2026-06-29 12:34:19 -0300

    feat(grafo): Implementa algoritmo de Tarjan para achar componentes conexos de forma eficiente
    Utiliza pilha e rastreamento de tempos de descoberta e retorno para isolar componentes fortemente conexos
    
    +refactor: Apaga função que retorna o hashmap interno grafo, pois isso quebra o encapsulamento proposta ao não declarar struct no .h

--- src/grafo.c
+++ src/grafo.c
@@ -31,6 +31,27 @@ typedef struct {
     void (*destructor_edge_data)(void *);
 } remove_edge_ctx_t;
 
+typedef struct stTarjanCtx {
+    int *discovery_time;
+    int *low_link;
+    bool *is_on_stack;
+    list_t *stack;
+    int timer;
+    int num_sccs;
+
+    exhash_t *id_to_index;
+    graph_t *g;
+
+    list_t *scc_list;
+    edge_filter_fn filter;
+    void *filter_ctx;
+} tarjan_ctx_t;
+
+typedef struct {
+    tarjan_ctx_t *tarjan;
+    int current_index;
+} tarjan_setup_ctx_t;
+
 // ======= STATIC DECLARATIONS ========
 
 static void remove_incoming_edges_cb(void *record_data, void *context);
@@ -38,6 +59,13 @@ static void destroy_graph_internal_cb(void *record_data, void *context);
 static void relax_edges(const char *u_id, double current_cost, dijkstra_ctx_t *ctx);
 static list_t *reconstruct_path(const char *src_id, const char *dst_id, exhash_t *parents);
 static int compare_target_id(void *target_edge, void *edge);
+
+static tarjan_ctx_t *tarjan_ctx_create(graph_t *g, edge_filter_fn filter, void *filter_ctx);
+static void tarjan_ctx_destroy(tarjan_ctx_t *ctx);
+static void map_vertex_tarjan_cb(const char *id, void *v_data, list_t *adj, void *context);
+static void tarjan_init_cb(const char *id, void *v_data, list_t *adj, void *context);
+static void tarjan_dfs(const char *curr_id, tarjan_ctx_t *ctx);
+static void destroy_parents_map(exhash_t *parents);
 
 // ======= GRAPH STRUCTS ========
 
@@ -69,7 +97,7 @@ graph_t *graph_init(
     graph_t *new_graph = malloc(sizeof(graph_t));
     assert(new_graph != NULL);
 
-    new_graph -> vertices = exhash_init(sizeof(vertex_t *), 512);
+    new_graph -> vertices = exhash_init(sizeof(vertex_t *), EXHASH_BUCKET_BYTES(sizeof(vertex_t *), 8));
     new_graph -> total_vertices = 0;
     new_graph -> destructor_edge_data = destructor_edge_data;
     new_graph -> destructor_vertex_data = destructor_vertex_data;
@@ -79,7 +107,7 @@ graph_t *graph_init(
 
 bool graph_add_vertex(graph_t *g, void *data, const char *id) {
     if (exhash_search(g -> vertices, id, NULL)) {
-        fprintf(stderr, "Vértice já existente no grafo. (grafo.c:%d)", __LINE__);
+        ERRO_LOG("Vértice já existente no grafo.");
         return false;
     }
 
@@ -156,7 +184,7 @@ vertex_t *graph_get_vertex(graph_t *g, const char *vertex_id) {
     vertex_t *wanted_vertex = NULL;
 
     if (!exhash_search(g -> vertices, vertex_id, &wanted_vertex)) {
-        fprintf(stderr, "Vértice de ID %s não encontrado. (grafo.c:%d)\n", vertex_id, __LINE__);
+        ERRO_LOG("Vértice de ID '%s' não encontrado.", vertex_id);
         return NULL;
     }
 
@@ -245,10 +273,6 @@ void graph_set_total_vertices(graph_t *g, int new_total_vertices) {
     g -> total_vertices = new_total_vertices;
 }
 
-exhash_t *graph_get_vertices_map(graph_t *g) {
-    return g -> vertices;
-}
-
 typedef struct {
     void (*user_callback)(const char *, void *, list_t *, void *);
     void *user_ctx;
@@ -283,6 +307,78 @@ const char *edge_get_target_id(edge_t *e) {
 void *edge_get_data(edge_t *e) {
     assert(e);
     return e -> data;
+}
+
+// ======= ALGORITHMS ========
+
+list_t *dijkstra(graph_t *graph, bool use_time, char *src_id, char *dst_id, double *out_cost) {
+    assert(graph != NULL);
+
+    dijkstra_ctx_t ctx = {
+        .graph = graph,
+        .costs = exhash_init(sizeof(double), EXHASH_BUCKET_BYTES(sizeof(double), 8)),
+        .parents = exhash_init(sizeof(char *), EXHASH_BUCKET_BYTES(sizeof(char *), 8)),
+        .min_heap = pq_init(16),
+        .use_time = use_time
+    };
+
+    double initial_cost = 0.0;
+    exhash_insert(ctx.costs, &initial_cost, src_id);
+    pq_enqueue(ctx.min_heap, src_id, 0);
+
+    while (!pq_is_empty(ctx.min_heap)) {
+        char *curr_id = pq_dequeue(ctx.min_heap);
+
+        if (strcmp(curr_id, dst_id) == 0) {
+            free(curr_id);
+            break;
+        }
+
+        double current_cost = 0.0;
+        exhash_search(ctx.costs, curr_id, &current_cost);
+
+        relax_edges(curr_id, current_cost, &ctx);
+
+        free(curr_id);
+    }
+
+    list_t *path = list_init();
+
+    if (exhash_search(ctx.costs, dst_id, NULL)) {
+        list_free(path, NULL);
+        path = reconstruct_path(src_id, dst_id, ctx.parents);
+
+        if (out_cost != NULL) {
+            exhash_search(ctx.costs, dst_id, out_cost);
+        }
+    }
+
+    pq_destroy(ctx.min_heap);
+    exhash_destroy(ctx.costs, NULL);
+    destroy_parents_map(ctx.parents);
+
+    return path;
+}
+
+list_t *tarjan(graph_t *g, edge_filter_fn filter, void *filter_ctx) {
+
+    // Aloca as estruturas
+    tarjan_ctx_t *ctx = tarjan_ctx_create(g, filter, filter_ctx);
+    tarjan_setup_ctx_t setup = { ctx, 0 };
+
+    // Mapeia os IDs (Strings) para Índices (Inteiros)
+    graph_foreach_vertex(g, map_vertex_tarjan_cb, &setup);
+
+    // Executa o DFS para cada vértice não visitado
+    graph_foreach_vertex(g, tarjan_init_cb, ctx);
+
+    // Salva o resultado antes de destruir o contexto
+    list_t *result_sccs = ctx -> scc_list;
+
+    // Libera a memória utilizada
+    tarjan_ctx_destroy(ctx);
+
+    return result_sccs;
 }
 
 // ======= STATIC IMPLEMENTATIONS ========
@@ -338,53 +434,33 @@ static void destroy_parents_map(exhash_t *parents) {
     exhash_destroy(parents, NULL);
 }
 
-list_t *dijkstra(graph_t *graph, bool use_time, char *src_id, char *dst_id, double *out_cost) {
-    assert(graph != NULL);
+static tarjan_ctx_t *tarjan_ctx_create(graph_t *g, edge_filter_fn filter, void *filter_ctx) {
+    int num_vertices = graph_get_total_vertices(g);
+    tarjan_ctx_t *ctx = malloc(sizeof(tarjan_ctx_t));
+    assert(ctx != NULL);
 
-    dijkstra_ctx_t ctx = {
-        .graph = graph,
-        .costs = exhash_init(sizeof(double), EXHASH_BUCKET_BYTES(sizeof(double), 8)),
-        .parents = exhash_init(sizeof(char *), EXHASH_BUCKET_BYTES(sizeof(char *), 8)),
-        .min_heap = pq_init(16),
-        .use_time = use_time
-    };
+    ctx -> discovery_time = malloc(sizeof(int) * num_vertices);
+    ctx -> low_link = malloc(sizeof(int) * num_vertices);
+    ctx -> is_on_stack = malloc(sizeof(bool) * num_vertices);
+    ctx -> stack = list_init();
+    ctx -> timer = 0;
+    ctx -> num_sccs = 0;
+    ctx -> id_to_index = exhash_init(sizeof(int), EXHASH_BUCKET_BYTES(sizeof(int), 8));
+    ctx -> g = g;
+    ctx -> scc_list = list_init();
+    ctx -> filter = filter;
+    ctx -> filter_ctx = filter_ctx;
 
-    double initial_cost = 0.0;
-    exhash_insert(ctx.costs, &initial_cost, src_id);
-    pq_enqueue(ctx.min_heap, src_id, 0);
+    return ctx;
+}
 
-    while (!pq_is_empty(ctx.min_heap)) {
-        char *curr_id = pq_dequeue(ctx.min_heap);
-
-        if (strcmp(curr_id, dst_id) == 0) {
-            free(curr_id);
-            break;
-        }
-
-        double current_cost = 0.0;
-        exhash_search(ctx.costs, curr_id, &current_cost);
-
-        relax_edges(curr_id, current_cost, &ctx);
-
-        free(curr_id);
-    }
-
-    list_t *path = list_init();
-
-    if (exhash_search(ctx.costs, dst_id, NULL)) {
-        list_free(path, NULL);
-        path = reconstruct_path(src_id, dst_id, ctx.parents);
-
-        if (out_cost != NULL) {
-            exhash_search(ctx.costs, dst_id, out_cost);
-        }
-    }
-
-    pq_destroy(ctx.min_heap);
-    exhash_destroy(ctx.costs, NULL);
-    destroy_parents_map(ctx.parents);
-
-    return path;
+static void tarjan_ctx_destroy(tarjan_ctx_t *ctx) {
+    free(ctx -> discovery_time);
+    free(ctx -> low_link);
+    free(ctx -> is_on_stack);
+    list_free(ctx -> stack, free);
+    exhash_destroy(ctx -> id_to_index, NULL);
+    free(ctx);
 }
 
 static void relax_edges(const char *u_id, double current_cost, dijkstra_ctx_t *ctx) {
@@ -438,4 +514,102 @@ static list_t *reconstruct_path(const char *src_id, const char *dst_id, exhash_t
     }
 
     return path;
+}
+
+static void map_vertex_tarjan_cb(const char *id, void *v_data, list_t *adj, void *context) {
+    (void)adj;
+    (void)v_data;
+
+    tarjan_setup_ctx_t *setup = context;
+    tarjan_ctx_t *tarjan_ctx = setup -> tarjan;
+
+    int idx = setup -> current_index;
+
+    exhash_insert(tarjan_ctx -> id_to_index, &idx, id);
+
+    tarjan_ctx -> discovery_time[idx] = -1;
+    tarjan_ctx -> is_on_stack[idx] = false;
+
+    setup -> current_index++;
+
+}
+
+static void tarjan_init_cb(const char *id, void *v_data, list_t *adj, void *context) {
+    (void)v_data;
+    (void)adj;
+    tarjan_ctx_t *ctx = context;
+    int idx = 0;
+    exhash_search(ctx -> id_to_index, id, &idx);
+
+    if (ctx -> discovery_time[idx] == -1) {
+        tarjan_dfs(id, ctx);
+    }
+}
+
+
+
+static void tarjan_dfs(const char *curr_id, tarjan_ctx_t *ctx) {
+    int curr_idx = 0;
+    exhash_search(ctx -> id_to_index, curr_id, &curr_idx);
+
+    ctx -> discovery_time[curr_idx] = ctx -> timer;
+    ctx -> low_link[curr_idx] = ctx -> timer;
+    ctx -> timer++;
+
+    list_push_front(ctx -> stack, my_strdup(curr_id));
+    ctx -> is_on_stack[curr_idx] = true;
+
+    list_t *neighbors = graph_get_neighbors(ctx -> g, curr_id);
+
+    for (list_node_t *node = list_node_front(neighbors); node != NULL; node = list_node_next(node)) {
+        edge_t *edge = list_node_data(node);
+        const char *neighbor_id = edge_get_target_id(edge);
+        void *street_data = edge_get_data(edge);
+
+
+
+        if (ctx -> filter(street_data, ctx -> filter_ctx)) {
+            int neighbor_idx = 0;
+
+            exhash_search(ctx -> id_to_index, neighbor_id, &neighbor_idx);
+
+            if (ctx -> discovery_time[neighbor_idx] == -1) {
+                tarjan_dfs(neighbor_id, ctx);
+
+                if (ctx -> low_link[neighbor_idx] < ctx -> low_link[curr_idx]) {
+                    ctx -> low_link[curr_idx] = ctx -> low_link[neighbor_idx];
+                }
+
+            } else if (ctx -> is_on_stack[neighbor_idx]) {
+                if (ctx -> discovery_time[neighbor_idx] < ctx -> low_link[curr_idx]) {
+                    ctx -> low_link[curr_idx] = ctx -> discovery_time[neighbor_idx];
+
+                }
+            }
+        }
+    }
+
+
+
+    if (ctx -> low_link[curr_idx] == ctx -> discovery_time[curr_idx]) {
+        list_t *scc = list_init();
+        char *popped_id;
+
+
+
+        do {
+            popped_id = list_pop_front(ctx -> stack);
+
+            int popped_idx = 0;
+            exhash_search(ctx -> id_to_index, popped_id, &popped_idx);
+
+            ctx -> is_on_stack[popped_idx] = false;
+            list_push_front(scc, popped_id);
+
+        } while (strcmp(popped_id, curr_id) != 0);
+
+
+        list_push_back(ctx -> scc_list, scc);
+        ctx -> num_sccs++;
+    }
 }
